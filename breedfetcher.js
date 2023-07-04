0d59cd08cb034f5488e710174163d09c8121ec5d
Haijoon Kim <abigail.haijoon.kim@gmail.com>
2023-07-03 20:00:52 -0700

    final update and npx mocha

--- breedfetcher.js
+++ breedfetcher.js
@@ -1,27 +1,20 @@
 const request = require('request');
-const arg = process.argv[2];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${arg}`, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    process.exit(1);
-  }
-  if (response.statusCode === 404) {
-    console.log(response.statusCode);
-    console.log('Page not found');
-    process.exit(1);
-  }
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+  
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback("Breed not found", null);
+      return;
+    }
 
-  if (body.length > 2) {
-    printResult(body);
-  } else {
-    console.log(`No matching result for ${arg}`);
-    process.exit(1);
-  }
-});
-
-const printResult = (body) => {
-  const fetchedData = JSON.parse(body);
-  console.log(typeof fetchedData);
-  console.log(fetchedData[0].description);
+    callback(null, data[0].description);
+  });
 };
+
+module.exports = { fetchBreedDescription };
